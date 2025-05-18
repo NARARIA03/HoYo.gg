d5be1b7db5a0272fdf9db5940ed1622d848b3824
NARARIA03 <hyeonseong0305@gmail.com>
2025-05-18 16:29:36 +0900

    feat: 원소 아이콘 추가 (#11)

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,26 +1,39 @@
+const landing = {
+  genshin: 'https://i.redd.it/66ic9c791ih61.png',
+  hsr: 'https://i.redd.it/zf4z6utfezua1.png',
+  zzz: 'https://i.redd.it/zenless-zone-zero-cbt2-images-v0-r3kvi8vt7cyb1.jpg?width=1080&crop=smart&auto=webp&s=93fb6ac323f6e407e60a7913820820e8977857aa',
+} as const;
+
+const genshin = {
+  emblem: {
+    몬드: 'https://i.namu.wiki/i/MO-6BaXU0D1N9q4xiFXVVhGWYAfzJfJF2zVUS1qnnTJ9-SXhZpjdxokMEB-qXTJ1Nt_QahsX4vE1HtB1WoHgPDazLEJq2_uaL5UiXhWq_5tEOm-oYxrCOmFB5BYRXDdFIyPHCzE9oOHece7aICZ34g.webp',
+    리월: 'https://i.namu.wiki/i/U_R9tJOkFatD2OYTvtTWhIRB9J6d3KO9RaEJd3iVnSpimPOPzk9NstYLZ35ficGwLKK7cjED8CKKbHHlHGZHwu2h2pFYeQxxxUwRLZeeiVoBLXdtox361zj4Mybbemsebe3DR4a02q0b-HsxNVm8Xg.webp',
+    이나즈마:
+      'https://i.namu.wiki/i/9ioRADDek3-cf37eb0fkk1yp-feb5NaXvyCCHjtuRJZj6WgPdgu3H8R1WMtAuaHY0fs9nsJlShsZSb360XuPaML6Dz2wlxXEsAFemS1D8CMglmiebmmDaZhMLpwsbgwww4IwGCHAL8OU8UUgT9EpUw.webp',
+    수메르:
+      'https://i.namu.wiki/i/Fz9ql2c5ufUM5AG_dt-onVChVx8_XB-biDB_d7rK8BeJpINVIncfsMIVOom3nJVkcQLYK3h-nVR_FU8Ry98t7IgmwXRL8THRf0gmaqLJIYNWc3n14JKRybQkTMSy1vRtxyZMuZDIN0XRwS35YFWwUw.webp',
+    폰타인:
+      'https://i.namu.wiki/i/anK9GkRLHyaeNVgQFkMYCEAmn_GqS0IGJO3G8GpALQVb9GT2KoPY7OYRAxrm7f1XXUP-y5t8tw5wD3RzoOY5_raCYyd5qJvJKW2FC6ursIlfPPiacOF-qt9IiTKAxT3XOz38RAIFbLNUpb6GPHkggQ.webp',
+    나타: 'https://i.namu.wiki/i/E7DL-O-rQkh0dgQL43G4bKCCzXmF872GImFbPUrlCLz-vYLCzSMn2QK_pQyfNOI8NzBmOmCOSIeqF5_rnWgi7IOYMzTaVS8jtrLFXbhsfYccfjVikrM6DobYK_vY17XwKZdjUTTOf8RIpL1bF3I7AA.webp',
+    스네즈나야:
+      'https://i.namu.wiki/i/OFCgI1choYxBXfkMXOkiSzJQkMNQwdHvogeMcxdoL7sp4ltpKJ70jYVHuaSMJR078UsWgB3ulU-_gdj33yqp6pbEXrd-3W-ed4zzgnDozegsKmH9xynLrWF-tWnl084ptGV02CDOAYNw4NumcCdIiQ.webp',
+  },
+  rarity: {
+    4: 'https://i.namu.wiki/i/NJhjPAjvk7xaS0vHcQ_5stwWn_qxRhycteX7CT5FBXsJK0wiOcdR3ZKg3u37IyP6yuII0YpNqyBLP7n9qmn6svQmS07OHdnQa-wPJNosPac1dvNHzILLf1YUzyopkNT-PL6Xyj4baWpthdsuXmXMQw.webp',
+    5: 'https://i.namu.wiki/i/KHFPTzDgsdL253_qUU1iUNRNRt7q24JTRkrYSrfzjh2frTTxoVYWHVBTcK-1PTY7zMmv-Nnds1UOxSSpTISEkaMnYm99WjQOzdQ_yGvrXmA8oceeM1-DcsvPRRL70SIBjXkRi5sH06mS_u-khJ5xSQ.webp',
+  },
+  element: {
+    바위: 'https://i.namu.wiki/i/NbN7Cg-kGqbtWQ7to9Lo9E5QySTnrzsqUW9p2ZFgLlrSxqLFoA1VBjDEZiURSzKBhGfO0i4x_ownorvaxmzGhwS5iwd8JwXZ6Hpeyvp3uxKxXOPW4jE1runnAwXnZsXwVXk6GbRGBfC-dqoIQTOJvQ.svg',
+    풀: 'https://i.namu.wiki/i/cMACAao4bqfBwdFlcCzQoMwz_et8k-vqFfB5VuiYxMNze53mq4ojXUPDb7txXC-QROybviM1Scsb8ZrLNBap0Kaxr10IHRgwrIABjIsd_8w8WgfQJ73FCs8An4BffA-eP-j1OUn3uBToNxuY_aN3tQ.svg',
+    얼음: 'https://i.namu.wiki/i/gPlMfRkowGILPwiI69p_HldgDK5Q8ykiD0ox7y3quJlc2NsIq7EX3X8UHazphCag5J6_pzRzBJByGCISUnWYC_LXSyZA74vJ7VAiAlbzVA15BHyPhqENnZSyuYaP7z2VKn0JWTjRapxNvL3TN3FiTw.svg',
+    불: 'https://i.namu.wiki/i/ZJBfeS2Bid5qC0JDl85zsouVXE8OfJI8zAv5rpQdLJjAADY2PcpQnlC23ZAL4bGiTg4N9w9z6yRhlJFAhFxuxBE_pzZLyDN6m9f2uQZMudgFZQ4sVLcbPTbYuCm9flr2xnz7C9lI6-f8-2x1snDqYg.svg',
+    물: 'https://i.namu.wiki/i/XEml6QQwHrSsYll1uuL5Fizfbdfib5mehgSycu-E-5b0nBuuQecEby6cnFo2bL2IsGPADN0sA6LZcuWv_B9ke31ZyRbXbxjsACqadwNOoP68RVVXNiZFqcXZyh2h8JhmG3PR7kKfokhC3ivyl_3x6Q.svg',
+    번개: 'https://i.namu.wiki/i/cJ7mP7eUKESu1JtWJQH3-mQClcP8XGySYwwJiyz2xA5tfOOUomTjd9T17yo8LDl0AiGlp3whcsnGOjcX-DC8PL9i7QLf0uoWl6PumvynyClsBSG2uFVq8SWXCDEt9UhCu3RDBGNPa7kYpuRSPNikaQ.svg',
+    바람: 'https://i.namu.wiki/i/gq5ZiI_nYuNElLbXfSdrlxKciqjgsaSvPfAS72ytoB3Cg3USmSB_nCGIOS5nh9UlF8n6fWPO6kO3Suvz6lTJqx5W5sgKpQoZQ07FqzRKgl2U4kJaBXtT0Ny_0TYjKKvpflmBp_9UPYWBu_jkAfXsdw.svg',
+  },
+} as const;
+
 export const IMAGES = {
-  landing: {
-    genshin: 'https://i.redd.it/66ic9c791ih61.png',
-    hsr: 'https://i.redd.it/zf4z6utfezua1.png',
-    zzz: 'https://i.redd.it/zenless-zone-zero-cbt2-images-v0-r3kvi8vt7cyb1.jpg?width=1080&crop=smart&auto=webp&s=93fb6ac323f6e407e60a7913820820e8977857aa',
-  },
-  genshin: {
-    emblem: {
-      몬드: 'https://i.namu.wiki/i/MO-6BaXU0D1N9q4xiFXVVhGWYAfzJfJF2zVUS1qnnTJ9-SXhZpjdxokMEB-qXTJ1Nt_QahsX4vE1HtB1WoHgPDazLEJq2_uaL5UiXhWq_5tEOm-oYxrCOmFB5BYRXDdFIyPHCzE9oOHece7aICZ34g.webp',
-      리월: 'https://i.namu.wiki/i/U_R9tJOkFatD2OYTvtTWhIRB9J6d3KO9RaEJd3iVnSpimPOPzk9NstYLZ35ficGwLKK7cjED8CKKbHHlHGZHwu2h2pFYeQxxxUwRLZeeiVoBLXdtox361zj4Mybbemsebe3DR4a02q0b-HsxNVm8Xg.webp',
-      이나즈마:
-        'https://i.namu.wiki/i/9ioRADDek3-cf37eb0fkk1yp-feb5NaXvyCCHjtuRJZj6WgPdgu3H8R1WMtAuaHY0fs9nsJlShsZSb360XuPaML6Dz2wlxXEsAFemS1D8CMglmiebmmDaZhMLpwsbgwww4IwGCHAL8OU8UUgT9EpUw.webp',
-      수메르:
-        'https://i.namu.wiki/i/Fz9ql2c5ufUM5AG_dt-onVChVx8_XB-biDB_d7rK8BeJpINVIncfsMIVOom3nJVkcQLYK3h-nVR_FU8Ry98t7IgmwXRL8THRf0gmaqLJIYNWc3n14JKRybQkTMSy1vRtxyZMuZDIN0XRwS35YFWwUw.webp',
-      폰타인:
-        'https://i.namu.wiki/i/anK9GkRLHyaeNVgQFkMYCEAmn_GqS0IGJO3G8GpALQVb9GT2KoPY7OYRAxrm7f1XXUP-y5t8tw5wD3RzoOY5_raCYyd5qJvJKW2FC6ursIlfPPiacOF-qt9IiTKAxT3XOz38RAIFbLNUpb6GPHkggQ.webp',
-      나타: 'https://i.namu.wiki/i/E7DL-O-rQkh0dgQL43G4bKCCzXmF872GImFbPUrlCLz-vYLCzSMn2QK_pQyfNOI8NzBmOmCOSIeqF5_rnWgi7IOYMzTaVS8jtrLFXbhsfYccfjVikrM6DobYK_vY17XwKZdjUTTOf8RIpL1bF3I7AA.webp',
-      스네즈나야:
-        'https://i.namu.wiki/i/OFCgI1choYxBXfkMXOkiSzJQkMNQwdHvogeMcxdoL7sp4ltpKJ70jYVHuaSMJR078UsWgB3ulU-_gdj33yqp6pbEXrd-3W-ed4zzgnDozegsKmH9xynLrWF-tWnl084ptGV02CDOAYNw4NumcCdIiQ.webp',
-    },
-    rarity: {
-      4: 'https://i.namu.wiki/i/NJhjPAjvk7xaS0vHcQ_5stwWn_qxRhycteX7CT5FBXsJK0wiOcdR3ZKg3u37IyP6yuII0YpNqyBLP7n9qmn6svQmS07OHdnQa-wPJNosPac1dvNHzILLf1YUzyopkNT-PL6Xyj4baWpthdsuXmXMQw.webp',
-      5: 'https://i.namu.wiki/i/KHFPTzDgsdL253_qUU1iUNRNRt7q24JTRkrYSrfzjh2frTTxoVYWHVBTcK-1PTY7zMmv-Nnds1UOxSSpTISEkaMnYm99WjQOzdQ_yGvrXmA8oceeM1-DcsvPRRL70SIBjXkRi5sH06mS_u-khJ5xSQ.webp',
-    },
-  },
+  landing,
+  genshin,
 } as const;
