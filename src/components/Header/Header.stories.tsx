71dcab93c241261f05aa221872e086fc46a81638
NARARIA03 <hyeonseong0305@gmail.com>
2025-06-30 02:00:29 +0900

    chore: storybook 수정 (#18)

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,10 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import styled from '@emotion/styled';
 import { Header } from './Header';
 
 const meta = {
   title: 'Components/Common/Header',
   component: Header,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <Wrapper>
+        <Story />
+        <Body>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti nobis ducimus. Ea quasi illum
+          voluptas, vel consequuntur eius excepturi eveniet temporibus quas, ipsam ad? Accusantium deserunt
+          reprehenderit odio eius?
+        </Body>
+      </Wrapper>
+    ),
+  ],
 } satisfies Meta<typeof Header>;
 
 export default meta;
@@ -20,3 +33,14 @@ export const Honkai_Starrail = {
 export const Zenless_Zone_Zero = {
   args: { game: 'zzz' },
 } satisfies StoryObj<typeof Header>;
+
+const Wrapper = styled.div`
+  background-color: rgb(30, 30, 47);
+  height: 200px;
+  overflow-y: auto;
+`;
+
+const Body = styled.div`
+  color: white;
+  font-size: 48px;
+`;
