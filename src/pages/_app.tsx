27cf0bdc80e08651d0e7dc54479fba5d4b7e8302
NARARIA03 <hyeonseong0305@gmail.com>
2025-07-01 23:29:21 +0900

    fix: 쿼리스트링 파싱 로직 타입 안전하게 개선 (#18)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,8 @@ import { DefaultSeoContainer } from '@/modules/seo';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Header } from '@/components';
-import { useRouter } from 'next/router';
 import { useInitQueryClient } from '@/hooks/useInitQueryClient';
+import { useParseGameQueryString } from '@/hooks/useParseGameQueryString';
 
 const pretendard = localFont({
   src: '../assets/fonts/PretendardVariable.woff2',
@@ -17,9 +17,7 @@ const pretendard = localFont({
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useInitQueryClient();
-  const router = useRouter();
-
-  const game = router.pathname.split('/')[1] as 'genshin' | 'hsr' | 'zzz' | '';
+  const game = useParseGameQueryString();
 
   return (
     <>
