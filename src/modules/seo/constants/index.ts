a3d6635b0efb21408e02de0a3c8f3fb52704d534
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-01 00:31:19 +0900

    fix: 타입이 너무 좁아져서 발생하는 빌드 에러 대응 (#29)

--- src/modules/seo/constants/index.ts
+++ src/modules/seo/constants/index.ts
@@ -1,4 +1,4 @@
-export const seoConstants = {
+export const seoConstants: Record<string, { title: string; description: string }> = {
   '/': {
     title: 'HoYo.gg',
     description: '호요버스 게임들의 데이터&진열장 검색 기능을 제공합니다.',
@@ -7,4 +7,4 @@ export const seoConstants = {
     title: 'Genshin.gg',
     description: '캐릭터, 성유물 등의 정보를 확인해보세요.',
   },
-} satisfies Record<string, { title: string; description: string }>;
+};
