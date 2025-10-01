ba7574432b509adb99cd4a13d806f6519cc6fe24
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-02 00:39:35 +0900

    fix: 환경변수가 누락된 경우, 빌드 타임 에러가 발생하도록 개선 (#29)

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,1 +1,6 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const getEnv = (key: string) => {
+  if (!process.env[key]) throw new Error(`${key} env is required.`);
+  return process.env[key];
+};
+
+export const BASE_URL = getEnv('NEXT_PUBLIC_BASE_URL');
