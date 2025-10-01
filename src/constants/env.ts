fe4ebf8c8d8ebb61ab1355311277a8dbcf4603c5
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-02 00:50:00 +0900

    fix: 환경 변수를 클라이언트에서 불러오지 못 하는 문제 수정 (#29)

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,6 @@
-const getEnv = (key: string) => {
-  if (!process.env[key]) throw new Error(`${key} env is required.`);
-  return process.env[key];
+const getEnv = (value?: string) => {
+  if (!value) throw new Error('환경 변수가 누락되었습니다.');
+  return value;
 };
 
-export const BASE_URL = getEnv('NEXT_PUBLIC_BASE_URL');
+export const BASE_URL = getEnv(process.env.NEXT_PUBLIC_BASE_URL);
