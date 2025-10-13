55bab2663371dd4d19a6009a26bfb0effe2b851e
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-13 22:43:42 +0900

    feat: naver-site-verification 메타태그 추가 (#44)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <meta name="google-site-verification" content="v4OwSDcDfg39cTXpFOLqNJVtF7zBYzt6PbShf0RI7dM" />
+        <meta name="naver-site-verification" content="6f78a927a786c7fdbb2f3189a7694d57e4d01748" />
       </Head>
       <body>
         <Main />
