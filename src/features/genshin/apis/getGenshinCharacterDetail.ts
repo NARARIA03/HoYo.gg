7184fb06e7586469854d2dfea503688d1a311529
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-17 00:45:48 +0900

    fix: 잘못되어있던 API DTO 수정 (#51)

--- src/features/genshin/apis/getGenshinCharacterDetail.ts
+++ src/features/genshin/apis/getGenshinCharacterDetail.ts
@@ -6,6 +6,6 @@ import type { GICharacterDetailDTO } from '../types/characterDetailDto';
 export const getGenshinCharacterDetail = async (characterId: string) => {
   const endpoint = getEndpoint(ENDPOINTS.genshin.characters.detail);
 
-  const { data } = await axios.get<GICharacterDetailDTO[]>(endpoint(characterId));
+  const { data } = await axios.get<GICharacterDetailDTO>(endpoint(characterId));
   return data;
 };
