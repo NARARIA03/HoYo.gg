cd59771231157219fd4e516ed44a2a9a61d3fa24
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-20 23:15:25 +0900

    refactor: genshin feature에 구현된 API 호출하도록 개선 (#53)

--- src/features/sitemap/apis/genshin.ts
+++ src/features/sitemap/apis/genshin.ts
@@ -1,13 +1,8 @@
-import axios from 'axios';
-import { ENDPOINTS } from '@/constants/endpoints';
 import { BASE_URL } from '@/constants/env';
-import { getEndpoint } from '@/utils';
-import type { GICharactersDTO } from '@/features/genshin/types/chatactersDto';
+import { getGenshinCharacters } from '@/features/genshin/apis/getGenshinCharacters';
 import { getGenshinDetailHref } from '@/features/genshin/utils/getGenshinHref';
 
 export const getGenshinCharacterDetailUrls = async () => {
-  const endpoint = getEndpoint(ENDPOINTS.genshin.characters.list);
-
-  const { data } = await axios.get<GICharactersDTO>(endpoint);
+  const data = await getGenshinCharacters();
   return Object.entries(data).map(([id, data]) => `${BASE_URL}${getGenshinDetailHref(data.KR, id)}`);
 };
